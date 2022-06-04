6cacf20f2241630c78b74a368209f29918ab401c
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-06-04 13:52:47 +0300

    Add files via upload

---
+++ EvenNumber_12.java
@@ -0,0 +1,21 @@
+package BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class EvenNumber_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        while (true){
+            if(num % 2 == 0){
+                System.out.printf("The number is: %d", Math.abs(num));
+                break;
+            }else{
+                System.out.println("Please write an even number.");
+                num = Integer.parseInt(scanner.nextLine());
+            }
+        }
+    }
+}
