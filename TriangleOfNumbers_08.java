30acded644b60f87132b0627408f78ff9072408d
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-06-12 17:13:40 +0300

    Add files via upload

---
+++ TriangleOfNumbers_08.java
@@ -0,0 +1,18 @@
+package BasicSyntaxConditionalStatementsAndLoopsExercise;
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int rows = 1; rows <= n; rows++) {
+            System.out.println();
+            for (int columns = 1; columns <= rows; columns++) {
+                System.out.print(rows + " ");
+            }
+        }
+    }
+}
